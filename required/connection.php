b00bcfc69ef498d6340b2c813f9a5ebb43b8994e
Praisecodes <cmancoder@gmail.com>
2022-12-22 15:49:57 +0100

    Hosting

--- required/connection.php
+++ required/connection.php
@@ -1,5 +1,13 @@
 <?php
-    $conn = new mysqli('localhost', 'root', '', 'nlacha');
+    // $conn = new mysqli('localhost', 'root', '', 'nlacha');
+
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     if($conn->connect_error){
         http_response_code(500);
